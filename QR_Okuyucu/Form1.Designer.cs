9c7b77cb70223182cd46fe0976b369e9f894b09d
Brkyoz <Brkyoz@DESKTOP-SFG19RH>
2025-11-23 07:11:16 +0300

    Add project files.

---
+++ QR_Okuyucu/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace QR_Okuyucu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnResimSec = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnyapistir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnResimSec
+            // 
+            this.btnResimSec.BackColor = System.Drawing.Color.Gold;
+            this.btnResimSec.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnResimSec.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnResimSec.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnResimSec.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.btnResimSec.Location = new System.Drawing.Point(12, 12);
+            this.btnResimSec.Name = "btnResimSec";
+            this.btnResimSec.Size = new System.Drawing.Size(140, 48);
+            this.btnResimSec.TabIndex = 0;
+            this.btnResimSec.Text = "Resim Seç";
+            this.btnResimSec.UseVisualStyleBackColor = false;
+            this.btnResimSec.Click += new System.EventHandler(this.btnResimSec_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(348, 187);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(92, 86);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightYellow;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(161, 366);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(467, 60);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnyapistir
+            // 
+            this.btnyapistir.BackColor = System.Drawing.Color.Gold;
+            this.btnyapistir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnyapistir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnyapistir.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnyapistir.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.btnyapistir.Location = new System.Drawing.Point(175, 13);
+            this.btnyapistir.Name = "btnyapistir";
+            this.btnyapistir.Size = new System.Drawing.Size(140, 47);
+            this.btnyapistir.TabIndex = 3;
+            this.btnyapistir.Text = "Yapıştır";
+            this.btnyapistir.UseVisualStyleBackColor = false;
+            this.btnyapistir.Click += new System.EventHandler(this.btnYapistir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnyapistir);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnResimSec);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Brkyoz_QR";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnResimSec;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnyapistir;
+    }
+}
+
---
+++ QR_Okuyucu/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Drawing; // Resim işlemleri
+using System.Windows.Forms;
+using ZXing; // Golcü (QR kütüphanesi)
+
+namespace QR_Okuyucu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        // ---------------------------------------------------------
+        // 1. BUTON: RESİM SEÇ (Dosyadan)
+        // ---------------------------------------------------------
+        private void btnResimSec_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dosyaAc = new OpenFileDialog();
+            dosyaAc.Filter = "Resim Dosyaları|*.jpg;*.jpeg;*.png;*.bmp|Tüm Dosyalar|*.*";
+            dosyaAc.Title = "QR Kodlu SS'i Seç Kral";
+
+            if (dosyaAc.ShowDialog() == DialogResult.OK)
+            {
+                // Seçilen resmi alıp ortak golcüye (QR_Coz) paslıyoruz
+                QR_Coz(Image.FromFile(dosyaAc.FileName));
+            }
+        }
+
+        // ---------------------------------------------------------
+        // 2. BUTON: YAPIŞTIR (Panodan / Ctrl+V)
+        // ---------------------------------------------------------
+        private void btnYapistir_Click(object sender, EventArgs e)
+        {
+            // Panoda resim var mı?
+            if (Clipboard.ContainsImage())
+            {
+                // Varsa resmi alıp ortak golcüye paslıyoruz
+                QR_Coz(Clipboard.GetImage());
+            }
+            else
+            {
+                MessageBox.Show("Kral panoda resim yok. Önce bir SS al (PrintScreen).", "Hafıza Boş");
+            }
+        }
+
+        // ---------------------------------------------------------
+        // ORTAK GOLCÜ (Teknik Direktörün Taktik Alanı)
+        // İki buton da işi buraya yıkar, burası çözer.
+        // ---------------------------------------------------------
+        private void QR_Coz(Image gelenResim)
+        {
+            try
+            {
+                // 1. Resmi ekrana bas
+                pictureBox1.Image = gelenResim;
+
+                // 2. Okumaya başla
+                BarcodeReader okuyucu = new BarcodeReader();
+                var sonuc = okuyucu.Decode((Bitmap)gelenResim);
+
+                // 3. Sonuç var mı?
+                if (sonuc != null)
+                {
+                    textBox1.Text = sonuc.Text;
+
+                    // Link kontrolü (Ofsayt taktiği)
+                    string link = sonuc.Text.ToLower();
+                    if (link.StartsWith("http") || link.StartsWith("www") || link.Contains(".com"))
+                    {
+                        // Tarayıcıyı aç
+                        System.Diagnostics.Process.Start(sonuc.Text);
+                    }
+                    else
+                    {
+                        // Link değilse sadece mesaj ver
+                        MessageBox.Show("QR Okundu: " + sonuc.Text);
+                    }
+                }
+                else
+                {
+                    textBox1.Text = "QR Bulunamadı.";
+                    MessageBox.Show("Resim var ama içinde QR kod yok kral.", "Ofsayt");
+                }
+            }
+            catch (Exception hata)
+            {
+                MessageBox.Show("Bir hata oldu kaptan: " + hata.Message);
+            }
+        }
+    }
+}
